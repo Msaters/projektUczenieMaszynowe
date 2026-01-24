6132ed95360b63151c6b15d719d3cc369e5d6caf
Milosz <85848694+Msaters@users.noreply.github.com>
2026-01-24 22:51:35 +0100

    open ai api and tf-idf bigrams embedings added
    embeddings finished more feature enginnering incoming

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ EMBEDED_FILEPATH = "winemag-data-130k-v2-tfidf-svd.csv"
 SVD_EMBEDDING = "embeddings.npy"
 CSV_FILEPATH = "winemag-data-130k-v2.csv"
 DIMENSIONS = 128
-MIN_WORD_OCCURENCE = 250
+MIN_WORD_OCCURENCE = 10
