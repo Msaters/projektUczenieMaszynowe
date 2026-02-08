2f856d2ed8a705f02da01cb2bc29eb29eb2cf48f
Milosz <85848694+Msaters@users.noreply.github.com>
2026-02-08 23:20:58 +0100

    in coming

--- fais_functions.py
+++ fais_functions.py
@@ -3,6 +3,7 @@ import numpy as np
 from config import *
 import pandas as pd
 import os
+from sklearn.preprocessing import MinMaxScaler
 
 # to do function for normal preprocess
 def fast_preprocess_df(df):
@@ -11,9 +12,21 @@ def fast_preprocess_df(df):
     df_nums = df_nums.astype('float32')
     return df_nums
 
-def find_similar_items(query_vector: np.ndarray, k: int):
+def find_similar_items(index, query_vector: np.ndarray, k: int):
+    query_vector = query_vector.astype('float32').reshape(1, -1)
+    faiss.normalize_L2(query_vector)
+
     D, I = index.search(query_vector.reshape(1, -1), k) # type: ignore
     return D, I
+
+def find_most_distant_items(index, query_vector: np.ndarray, k: int):
+    query_vector = query_vector.astype('float32').reshape(1, -1)
+    faiss.normalize_L2(query_vector)
+    
+    opposite_vector = -query_vector
+    D, I = index.search(opposite_vector, k)
+    return D, I
+
 
 def get_wines_by_indices(indices: np.ndarray, df: pd.DataFrame):
     wines_reviews_df = df.iloc[indices.flatten()]
@@ -21,6 +34,22 @@ def get_wines_by_indices(indices: np.ndarray, df: pd.DataFrame):
     print(descriptions)
     return wines_reviews_df
 
+def prepare_weighted_features(df, weight_dict=None, default_weight=5.0):
+    all_cols = df.columns.tolist()
+    embedding_cols = [c for c in all_cols if str(c).isdigit() or str(c).startswith('svd_')]
+    feature_cols = [c for c in all_cols if c not in embedding_cols]
+
+    scaler = MinMaxScaler()
+    df_scaled = pd.DataFrame(scaler.fit_transform(df), columns=all_cols, index=df.index)
+
+    if weight_dict:
+        for col, w in weight_dict.items():
+            if col in df_scaled.columns:
+                df_scaled[col] *= w
+    else:
+        df_scaled[feature_cols] *= default_weight
+
+    return df_scaled.astype('float32')
 
 def build_faiss_index_from_df_nums(df_nums: pd.DataFrame): 
     df_nums_array = df_nums.to_numpy()
@@ -29,7 +58,7 @@ def build_faiss_index_from_df_nums(df_nums: pd.DataFrame):
 
     if len(indeksy_zerowe) > 0:
         print(f"Uwaga: Znaleziono {len(indeksy_zerowe)} win z pustymi wektorami.")
-        # Rozwiązanie: Dodajemy minimalną wartość (epsilon), żeby uniknąć dzielenia przez 0
+        # Dodajemy minimalną wartość (epsilon), żeby uniknąć dzielenia przez 0
         # Dzięki temu wektor będzie "prawie zerowy", ale normalizacja zadziała.
         df_nums_array[indeksy_zerowe] += 1e-10
 
@@ -56,3 +85,10 @@ def append_embedding_to_file(filepath, new_embedding):
     else:
         # Jeśli plik nie istnieje zapisz wektor
         np.save(filepath, new_vector)
+
+def save_index(index, fileName):
+    faiss.write_index(index, fileName)
+
+def faiss_read(index, fileName):
+    index = faiss.read_index("wine_index.faiss")
+    return index
